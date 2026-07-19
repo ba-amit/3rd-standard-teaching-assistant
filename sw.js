6354b9220efad66e523db9614e6febc5bfee1bdb
ad <amit@buyerassist.io>
2026-07-19 14:09:09 +0530

    Fix broken Google TTS endpoint (translate.google.com deprecated its token-free route)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "varnamala-v1";
+const CACHE = "varnamala-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
